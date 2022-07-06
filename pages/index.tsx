1a4c65ed93d4e67f73cd94d47484ecc70fec76c6
mehmetvarol <mehmt.varol@gmail.com>
2022-07-06 11:26:45 +0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ response }: any) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,12 +13,13 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <div>{JSON.stringify(response)}</div>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +60,27 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const datas = await fetch(
+    `http://test.gateway.api:7001/pages/housingloan/home`
+  );
+  const response = await datas.json();
+
+  return {
+    props: {
+      response,
+    },
+  };
+};
+
+export default Home;
